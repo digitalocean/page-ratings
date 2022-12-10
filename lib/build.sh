5bc4b3ebc799e31820e6040c67c19d5d1da9ca17
John Mulhausen <jmulhausen@digitalocean.com>
2022-12-09 23:27:44 -0500

    build.sh stuff

--- lib/build.sh
+++ lib/build.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 # Install dependencies
 cd mysql-connector-python
-apt-get install software-properties-common
-add-apt-repository ppa:deadsnakes/ppa
-sudo apt-get update
-sudo apt-get install python3.8
-python setup.py install
+#python setup.py install
