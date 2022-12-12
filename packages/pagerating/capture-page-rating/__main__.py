dd83c1dc567fba3c9f79fb61554f2132b243fe36
John Mulhausen <jmulhausen@digitalocean.com>
2022-12-11 23:21:58 -0500

    Update __main__.py

--- packages/pagerating/capture-page-rating/__main__.py
+++ packages/pagerating/capture-page-rating/__main__.py
@@ -1,7 +1,7 @@
 import os
 import mysql.connector
 
-def printOut(message):
+def print_out(message):
     return {
         'body': {
             'response_type': 'in_channel',
@@ -21,7 +21,7 @@ def main(args):
         sql = "INSERT INTO ratings (IPAddress, Rating, URL) VALUES ('255.255.255.255', 75, 'https://example.com')"
         mycursor.execute(sql)
         mydb.commit()
-        printOut(mycursor.rowcount + "record inserted into " + os.getenv('DB_HOST') + ":" + os.getenv('DB_DATABASE'))
+        print_out(mycursor.rowcount + "record inserted into " + os.getenv('DB_HOST') + ":" + os.getenv('DB_DATABASE'))
         mycursor.close()
     except mysql.connector.Error as error:
-        printOut("Failed to insert record into " + os.getenv('DB_HOST') + ":" + os.getenv('DB_DATABASE') + ": {}".format(error))
+        print_out("Failed to insert record into " + str(os.getenv('DB_HOST')) + ":" + str(os.getenv('DB_DATABASE')) + ": " + str(error))
